323d5cd651ec1e2996ac990ec0bd9de9b769fce9
salahbeeh <mohammedghrably98@gmail.com>
2022-07-02 15:31:01 +0000

    adding ansible

---
+++ prepare_inventory.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+aws ec2 describe-instances \
+\
+        --query 'Reservations[*].Instances[*].PublicIpAddress' \
+      --filters "Name=tag:Project,Values=udacity" \
+      --output text >> inventory
